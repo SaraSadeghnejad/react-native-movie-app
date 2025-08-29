12934cff1ef16ba9cd527797e3b5d877f56076e2
sara <sarasadeghnejad33@gmail.com>
2025-08-29 18:38:35 +0330

    first commit

---
+++ app/movie/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieName = () => {
+  return (
+    <View>
+      <Text>MovieName</Text>
+    </View>
+  )
+}
+
+export default MovieName
